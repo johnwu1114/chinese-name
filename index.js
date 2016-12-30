6afd501830241b6c56fddff8a46259c1ba87e496
John Wu <me@johnwu.cc>
2016-12-31 02:24:28 +0800

    新增生肖喜忌字, 新增三才解說

--- index.js
+++ index.js
@@ -1,103 +1,123 @@
-//ref: http://www.dearmoney.classv.tw/name/result_babyname_free
-var $quality = 40;
+var $api_url = "https://johnwu1114.github.io/ChineseName/";
+var $quality = 80;
 var $pickWords = [];
-var $kangxi;
-var $5EKey = ["水", "木", "木", "火", "火", "土", "土", "金", "金", "水"];
-var $5E;
+var $chineseCharacters;
+var $sancaiKey = ["水", "木", "木", "火", "火", "土", "土", "金", "金", "水"];
+var $sancai;
 var $81;
 
 $(function () {
-    var topDrawCount = 0;
-    var top5E = 0;
-
     $(this).on("change", "#familyName", function () {
         if ($(this).val() == "") return;
-        $(".item").find("option").remove();
-        $(".item").append("<option>請選擇</option>");
+        $("#combination").find("option").remove();
+        $("#combination").append("<option value=''>請選擇</option>");
 
-        for (var key in $kangxi) {
-            if ($kangxi[key].words.indexOf($("#familyName").val()) != -1) {
-                topDrawCount = $kangxi[key].draw;
-                top5E = (topDrawCount + 1) % 10;
-                $(".familyName").html($("#familyName").val());
-                $(".familyNameDrawCount").html(topDrawCount);
-                break;
-            }
-        }
+        var results = getCombinations($("#familyName").val());
 
-        for (var key in $5E) {
-            if (key[0] == $5EKey[top5E] && $5E[key].value >= 8) {
-                for (var middleCount = topDrawCount + 1; middleCount <= topDrawCount + 26; middleCount++) {
-                    var middleFive = middleCount % 10;
-                    if ($5EKey[middleFive] == key[1]) {
-                        var middleDrawCount = middleCount - topDrawCount;
-                        for (var bottomCount = middleDrawCount + 1; bottomCount <= middleDrawCount + 26; bottomCount++) {
-                            var bottomFive = bottomCount % 10;
-                            if ($5EKey[bottomFive] == key[2]) {
-                                var bottomDrawCount = bottomCount - middleDrawCount;
+        results.sort(function (a, b) {
+            if (a.value < b.value)
+                return 1;
+            if (a.value > b.value)
+                return -1;
+            if (a.middle > b.middle)
+                return 1;
+            if (a.middle < b.middle)
+                return -1;
+            if (a.bottom > b.bottom)
+                return 1;
+            if (a.bottom < b.bottom)
+                return -1;
+            return -1;
+        });
 
-                                var value = $81[topDrawCount + 1].value;
-                                value += $81[topDrawCount + middleDrawCount].value;
-                                value += $81[middleDrawCount + bottomDrawCount].value;
-                                value += $81[bottomDrawCount + 1].value;
-                                value += $81[topDrawCount + middleDrawCount + bottomDrawCount].value;
-
-                                if (value >= Math.min(Math.max(0, $quality), 50)) {
-                                    var str = "適合筆畫: " + topDrawCount + ", " + middleDrawCount + ", " + bottomDrawCount + " "
-                                    + "(" + key + " " + $5E[key].text + ")";
-                                    $(".item").append($("<option></option>").attr("value", '{"middle":' + middleDrawCount + ', "bottom":' + bottomDrawCount + '}').text(str));
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        for (key in results) {
+            var item = results[key];
+            var str = "適合筆畫: " + item.top + ", " + item.middle + ", " + item.bottom + " " + "(綜合分數:" + item.value + ")";
+            $("#combination").append($("<option></option>").attr("value", JSON.stringify(item)).text(str));
         }
     });
 
-    $(this).on("change", ".item", function () {
+    $(this).on("change", "#combination", function () {
+        if ($(this).val() == "") return;
         var val = $.parseJSON($(this).val());
 
         $pickWords = [];
-        for (var key in $kangxi) {
-            if ($kangxi[key].draw == val.middle
-            || $kangxi[key].draw == val.bottom)
-                $pickWords.push($kangxi[key]);
+        for (var key in $chineseCharacters) {
+            if ($chineseCharacters[key].draw == val.middle
+            || $chineseCharacters[key].draw == val.bottom)
+                $pickWords.push($chineseCharacters[key]);
         }
+        var draw = 0;
 
-        $(".topFiveElement").html((topDrawCount + 1) + " " + get5EColor((topDrawCount + 1) % 10));
-        $(".topContent").html(get81Content(topDrawCount + 1));
+        $(".sancai").html(val.key);
+        $(".sancaiGoodOrbad").html($sancai[val.key].text);
+        $(".sancaiContent").html($sancai[val.key].content);
 
-        $(".middleFiveElement").html((topDrawCount + val.middle) + " " + get5EColor((topDrawCount + val.middle) % 10));
-        $(".middleContent").html(get81Content(topDrawCount + val.middle));
+        draw = val.top + 1;
+        renderResult("top", draw);
 
-        $(".bottomFiveElement").html((val.middle + val.bottom) + " " + get5EColor((val.middle + val.bottom) % 10));
-        $(".bottomContent").html(get81Content(val.middle + val.bottom));
+        draw = val.top + val.middle;
+        renderResult("middle", draw);
 
-        $(".outFiveElement").html((val.bottom + 1) + " " + get5EColor((val.bottom + 1) % 10));
-        $(".outContent").html(get81Content(val.bottom + 1));
+        draw = val.middle + val.bottom;
+        renderResult("bottom", draw);
 
-        $(".allFiveElement").html((topDrawCount + val.middle + val.bottom) + " " + get5EColor((topDrawCount + val.middle + val.bottom) % 10));
-        $(".allContent").html(get81Content(topDrawCount + val.middle + val.bottom));
+        draw = val.bottom + 1;
+        renderResult("out", draw);
 
-        $.get("https://johnwu1114.github.io/ChineseName/" + $("#zodiac").val() + ".json", function (data) {
+        draw = val.top + val.middle + val.bottom;
+        renderResult("total", draw);
+
+        $.get($api_url + $("#zodiac").val() + ".json", function (data) {
             $(".giveNameDrawCount1").html(val.middle);
-            $(".giveName1").html(getWordsOf5E(data.better["_" + val.middle]));
+            $(".giveName1_better").html(getWordsOf5E(data.better["_" + val.middle]));
+            $(".giveName1_worse").html(getWordsOf5E(data.worse["_" + val.middle]));
+
+            var normal = "";
+            for (var key in $chineseCharacters) {
+                if ($chineseCharacters[key].draw === val.middle) {
+                    var chars = $chineseCharacters[key].chars;
+                    var i = chars.length;
+                    while (i--) {
+                        if ((!data.better["_" + val.middle] || data.better["_" + val.middle].indexOf(chars[i]) == -1)
+                         && (!data.worse["_" + val.middle] || data.worse["_" + val.middle].indexOf(chars[i]) == -1))
+                            normal += chars[i];
+                    }
+                }
+            }
+
+            $(".giveName1_normal").html(getWordsOf5E(normal));
 
             $(".giveNameDrawCount2").html(val.bottom);
-            $(".giveName2").html(getWordsOf5E(data.better["_" + val.bottom]));
+            $(".giveName2_better").html(getWordsOf5E(data.better["_" + val.bottom]));
+            $(".giveName2_worse").html(getWordsOf5E(data.worse["_" + val.bottom]));
         });
     });
 
-    $.get("https://johnwu1114.github.io/ChineseName/KangXi.json", function (data) {
-        $kangxi = data;
+    $(this).on("change", "#zodiac", function () {
+        $("#familyName").trigger("change");
     });
 
-    $.get("https://johnwu1114.github.io/ChineseName/FiveElements.json", function (data) {
-        $5E = data;
+    $(this).on("click", ".btnDisplay", function () {
+        if ($(this).text() == "顯示") {
+            $(this).text("隱藏")
+            $(this).closest("tr").find("span").removeClass("hide");
+        } else {
+            $(this).text("顯示")
+            $(this).closest("tr").find("span").addClass("hide");
+        }
     });
 
-    $.get("https://johnwu1114.github.io/ChineseName/EightyOne.json", function (data) {
+    //$.get($api_url + "ChineseCharacters.json", function (data) {
+    $.get($api_url + "KangXi.json", function (data) {
+        $chineseCharacters = data;
+    });
+
+    $.get($api_url + "Sancai.json", function (data) {
+        $sancai = data;
+    });
+
+    $.get($api_url + "EightyOne.json", function (data) {
         $81 = {};
         for (var key in data) {
             $81[data[key].draw] = data[key];
@@ -105,26 +125,33 @@ $(function () {
     });
 });
 
-function get81Content(draw) {
-    return "<b>[" + $81[draw].text + "]</b> " + $81[draw].content;
+function renderResult(type, draw) {
+    $("." + type + "DrawCount").html(draw);
+    $("." + type + "5e").html(get5EColor((draw) % 10));
+    $("." + type + "GoodOrbad").html($81[draw].text);
+    $("." + type + "Content").html(get81Content(draw));
 }
 
-function getWordsOf5E(words) {
-    var str = "";
-    if (words) {
-        for (var i = 0; i < words.length; i++) {
+function get81Content(draw) {
+    return $81[draw].content;
+}
+
+function getWordsOf5E(chars) {
+    var arr = [];
+    if (chars) {
+        for (var i = 0; i < chars.length; i++) {
             for (var key in $pickWords) {
-                if ($pickWords[key].words.indexOf(words[i]) != -1) {
-                    str += words[i] + get5EColor($pickWords[key].five) + ", ";
+                if ($pickWords[key].chars.indexOf(chars[i]) != -1) {
+                    arr.push(chars[i] + get5EColor($pickWords[key].fiveEle));
                 }
             }
         }
     }
-    return str;
+    return arr.join(", ");
 }
 
-function get5EColor(five) {
-    switch (five) {
+function get5EColor(fiveEle) {
+    switch (fiveEle) {
         case "木":
         case 1:
         case 2:
@@ -146,4 +173,55 @@ function get5EColor(five) {
         case 9:
             return "<b style='color:blue'>(水)</b>";
     }
+}
+
+function getCombinations(familyName) {
+    var topDrawCount = 0;
+    var top5E = 0;
+
+    for (var key in $chineseCharacters) {
+        if ($chineseCharacters[key].chars.indexOf(familyName) != -1) {
+            topDrawCount = $chineseCharacters[key].draw;
+            top5E = (topDrawCount + 1) % 10;
+            $(".familyName").html(familyName + get5EColor($chineseCharacters[key].fiveEle));
+            $(".familyNameDrawCount").html(topDrawCount);
+            break;
+        }
+    }
+
+    var results = [];
+    for (var key in $sancai) {
+        if (key[0] == $sancaiKey[top5E] && $sancai[key].value >= 8) {
+            for (var middleCount = topDrawCount + 1; middleCount <= topDrawCount + 26; middleCount++) {
+                var middleFive = middleCount % 10;
+                if ($sancaiKey[middleFive] == key[1]) {
+                    var middleDrawCount = middleCount - topDrawCount;
+                    for (var bottomCount = middleDrawCount + 1; bottomCount <= middleDrawCount + 26; bottomCount++) {
+                        var bottomFive = bottomCount % 10;
+                        if ($sancaiKey[bottomFive] == key[2]) {
+                            var bottomDrawCount = bottomCount - middleDrawCount;
+
+                            var value = $81[topDrawCount + 1].value;
+                            value += $81[topDrawCount + middleDrawCount].value;
+                            value += $81[middleDrawCount + bottomDrawCount].value;
+                            value += $81[bottomDrawCount + 1].value;
+                            value += $81[topDrawCount + middleDrawCount + bottomDrawCount].value;
+                            value *= 2;
+                            if (value >= Math.min(Math.max(0, $quality), 100)) {
+                                results.push({
+                                    "key": key,
+                                    "value": value,
+                                    "top": topDrawCount,
+                                    "middle": middleDrawCount,
+                                    "bottom": bottomDrawCount
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return results;
 }
